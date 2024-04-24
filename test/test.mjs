a0911e64d10a56213c0fb6ef08c59a3f982b1110
GitHub30 <funaox@gmail.com>
2024-04-24 15:42:50 +0000

    update

--- test/test.mjs
+++ test/test.mjs
@@ -1,36 +1,40 @@
 import { readFileSync } from 'node:fs';
 import assert from 'assert';
 
-
-console.log(eval(readFileSync(import.meta.dirname + '/../KiZooNa.js').toString()))
-
-console.log(global)
+eval(readFileSync('KiZooNa.js') + ';global.DB=DB;global.Table=Table')
 
 describe('DATABASE', function () {
   let db
   before(function () {
-    // db = new DB({
-    //   url: 'http://localhost/sql-injection.php',
-    //   dsn: 'mysql:host=127.0.0.1;dbname=mariadb',
-    //   username: 'mariadb',
-    //   password: 'mariadb',
-    //   debug: true
-    // })
+    db = new DB({
+      url: 'http://localhost/sql-injection.php',
+      dsn: 'mysql:host=127.0.0.1;dbname=mariadb',
+      username: 'mariadb',
+      password: 'mariadb',
+      debug: true
+    })
   });
 
-  it('Create table', function () {
-    assert.equal([1, 2, 3].indexOf(4), -1);
+  it('Create table', async function () {
+    await db.createTable('users', table => {
+      table.increments('id')
+      table.string('name', 255).nullable().default(null)
+      table.integer('age').nullable().default(null)
+      table.timestamp('created_at').useCurrent()
+      table.timestamp('updated_at').useCurrent().useCurrentOnUpdate()
+      table.softDeletes()
+    })
   });
-  it('Insert', function () {
-    assert.equal([1, 2, 3].indexOf(4), -1);
+  it('Insert', async function () {
+    await db.table('users').insert({ name: 'Jake', age: 29 })
   });
-  it('Update', function () {
-    assert.equal([1, 2, 3].indexOf(4), -1);
+  it('Update', async function () {
+    await db.table('users').where('name', 'Jake').update({ age: 31 })
   });
-  it('Delete', function () {
-    assert.equal([1, 2, 3].indexOf(4), -1);
+  it('Delete', async function () {
+    await db.table('users').where('name', 'Jake').delete()
   });
-  it('Drop table', function () {
-    assert.equal([1, 2, 3].indexOf(4), -1);
+  it('Drop table', async function () {
+    await db.dropTable('users')
   });
 });
